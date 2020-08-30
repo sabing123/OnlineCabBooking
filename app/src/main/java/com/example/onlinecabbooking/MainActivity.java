4cbc94318ca5c664df5528ea11521378154a2d87
Sabin <sabingautam05@gmail.com>
2020-08-30 21:30:16 +0545

    Completing Current location of driver section

--- app/src/main/java/com/example/onlinecabbooking/MainActivity.java
+++ app/src/main/java/com/example/onlinecabbooking/MainActivity.java
@@ -5,40 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
+import com.example.onlinecabbooking.driverui.DriverLoginRegisterActivity;
+import com.example.onlinecabbooking.passengerui.CustomerLoginRegisterActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private Button signin,signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        signin = findViewById(R.id.signin);
-        signup = findViewById(R.id.signup);
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null)
-        {
-            Intent intent = new Intent(MainActivity.this,MainPageActivity.class);
-            startActivity(intent);
-            finish();
-        }
-        else {
-
-            setContentView(R.layout.activity_main);
-        }
+        setContentView(R.layout.activity_main);
+//        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//        if (user != null)
+//        {
+//            Intent intent = new Intent(MainActivity.this,MainPageActivity.class);
+//            startActivity(intent);
+//            finish();
+//        }
+//        else {
+//
+//
+//        }
     }
 
 
-    public void open_signin(View V){
-        startActivity(new Intent(MainActivity.this, SigninActivity.class));
+    public void open_driver(View V){
+        startActivity(new Intent(MainActivity.this, DriverLoginRegisterActivity.class));
     }
 
-    public void open_signup(View V){
-        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+    public void open_passenger(View V){
+        startActivity(new Intent(MainActivity.this, CustomerLoginRegisterActivity.class));
     }
 
 
---
+++ app/src/main/java/com/example/onlinecabbooking/driverui/DriverMapsActivity.java
@@ -0,0 +1,102 @@
+package com.example.onlinecabbooking.driverui;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.onlinecabbooking.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class DriverMapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+
+    private GoogleMap mMap;
+    GoogleApiClient googleApiClient;
+    Location lastlocation;
+    LocationRequest locationRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        buildGoogleAPIClient();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+        lastlocation = location;
+
+        LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(locationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest,this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    protected synchronized void buildGoogleAPIClient(){
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        googleApiClient.connect();
+
+    }
+
+}
